d45dc7f73583d2de137f4c85c20636596ed728f0
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-01 00:15:38 -0400

    Starting to flesh out

--- algnums.py
+++ algnums.py
@@ -3,6 +3,18 @@ class AlgebraicInteger :
     def __init__(self) :
         print('PLACEHOLDER 3')
 
+    # TODO: Write tests for this
+    def algebraic_degree() :
+        return -1
+
+    # TODO: Write tests for this
+    def trace() :
+        return -3
+
+    # TODO: Write tests for this
+    def norm() :
+        return -5
+
 class QuadraticInteger(AlgebraicInteger) :
 
     def __init__(self, ring) :
