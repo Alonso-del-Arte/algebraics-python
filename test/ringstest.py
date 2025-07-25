6500244d0372def04cb63e70ff96a85c1df0bce3
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-24 21:27:45 -0400

    Failing max_degree( ) test

--- test/ringstest.py
+++ test/ringstest.py
@@ -1,3 +1,13 @@
 import unittest
+from src.rings import IntegerRing
 
-println("Placeholder for tests of rings.py")
+class TestIntegerRing(unittest.TestCase) :
+    
+    def test_max_degree(self) :
+        instance = IntegerRing()
+        expected = 1
+        actual = instance.max_degree
+        self.assertEqual(actual, expected)
+        
+if __name__ == '__main__' :
+    unittest.main()
