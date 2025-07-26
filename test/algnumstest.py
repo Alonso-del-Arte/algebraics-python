1df78720bb02bce6c7d68717193bfcbcaecd74b2
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-25 23:11:47 -0400

    Failing __str__( ) test

--- test/algnumstest.py
+++ test/algnumstest.py
@@ -1,3 +1,16 @@
+import random
 import unittest
 
-println("Placeholder for tests of algnums.py")
+from src.algnums import AlgebraicInteger
+
+class AlgebraicIntegerTest(unittest.TestCase) :
+    
+    def test_str(self) :
+        n = random.randrange(-32768, 32767)
+        instance = AlgebraicInteger(n)
+        expected = str(n)
+        actual = instance.__str__()
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__' :
+    unittest.main()
