40452b6c16a76b517c2302e783542d993b560e71
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-08 10:10:41 -0400

    Passing negative not squarefree test

--- src/numth.py
+++ src/numth.py
@@ -9,7 +9,16 @@ def squarefree(num) :
     if num == -1 :
         return True
     if num < -1 :
-        return True
+        if num % 4 == 0 :
+            return False
+        threshold = math.sqrt(abs(num))
+        all_distinct_so_far = True
+        root = 3
+        while all_distinct_so_far and root <= threshold :
+            square = root * root
+            all_distinct_so_far = (num % square != 0)
+            root += 2
+        return all_distinct_so_far
     if num % 4 == 0 :
         return False
     threshold = math.sqrt(abs(num))
