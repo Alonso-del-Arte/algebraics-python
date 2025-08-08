f5eba389d8f0229989d2e1c350d6451742230805
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-08 10:11:25 -0400

    Refactored

--- src/numth.py
+++ src/numth.py
@@ -8,17 +8,6 @@ def squarefree(num) :
         return False
     if num == -1 :
         return True
-    if num < -1 :
-        if num % 4 == 0 :
-            return False
-        threshold = math.sqrt(abs(num))
-        all_distinct_so_far = True
-        root = 3
-        while all_distinct_so_far and root <= threshold :
-            square = root * root
-            all_distinct_so_far = (num % square != 0)
-            root += 2
-        return all_distinct_so_far
     if num % 4 == 0 :
         return False
     threshold = math.sqrt(abs(num))
