015d7da4c01bb2ba83536cac38a51f31a0ffa69f
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-18 22:19:04 -0400

    New class and test class

---
+++ src/fractions.py
@@ -0,0 +1,37 @@
+# TODO: Write tests for this
+def euclidean_gcd(a, b) :
+    return -1
+
+class Fraction :
+    
+    def __init__(self, numer, denom) :
+        self.numerator = numer
+        self.denominator = denom
+
+    # TODO: Write tests for this
+    def HTML_str(self) :
+        return "SORRY, NOT IMPLEMENTED YET"
+    
+    # TODO: Write tests for this
+    def TeX_str(self) :
+        return "SORRY, NOT IMPLEMENTED YET"
+
+    # TODO: Write tests for this
+    def __add__(self, addend) :
+        return self
+
+    # TODO: Write tests for this
+    def __neg__(self, divisor) :
+        return self
+
+    # TODO: Write tests for this
+    def __sub__(self, subtrahend) :
+        return self
+
+    # TODO: Write tests for this
+    def __mul__(self, multiplicand) :
+        return self
+
+    # TODO: Write tests for this
+    def __floordiv__(self, divisor) :
+        return self
---
+++ test/fractionstest.py
@@ -0,0 +1,14 @@
+import random
+import unittest
+
+import src.fractions
+
+class FractionTest(unittest.TestCase) :
+    
+    def test_euclidean_gcd_same_number(self) :
+        a = random.randrange(-32768, 32767)
+        self.fail("PLACEHOLDER")
+
+if __name__ == '__main__' :
+    unittest.main()
+    
