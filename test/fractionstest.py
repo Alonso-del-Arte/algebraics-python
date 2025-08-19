8f7f9c478d47858c7b5019050b7aa6246ed5ca51
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-18 22:24:04 -0400

    Failing GCD same positive number test

--- test/fractionstest.py
+++ test/fractionstest.py
@@ -6,8 +6,11 @@ import src.fractions
 class FractionTest(unittest.TestCase) :
     
     def test_euclidean_gcd_same_number(self) :
-        a = random.randrange(-32768, 32767)
-        self.fail("PLACEHOLDER")
+        expected = random.randrange(1, 32767)
+        actual = src.fractions.euclidean_gcd(expected, expected)
+        num_str = str(expected)
+        msg = "Reckoning gcd(" + num_str + ", " + num_str + ")"
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__' :
     unittest.main()
