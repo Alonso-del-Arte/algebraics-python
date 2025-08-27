f83ca0a4195bc3ebd9ed6887bc85630ec7a5e2a0
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-27 08:22:36 -0400

    Refactored __neg__( ) to not treat zero as a special case

--- src/fractions.py
+++ src/fractions.py
@@ -42,8 +42,6 @@ class Fraction :
         return Fraction(numer, denom)
 
     def __neg__(self) :
-        if self.numerator == 0 :
-            return self
         return Fraction(-self.numerator, self.denominator)
 
     def __sub__(self, subtrahend) :
