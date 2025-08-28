aec54475ac364890a6b7bf83585a462e3732a81c
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-27 23:40:10 -0400

    Failing __str__( ) test for negative fractions

--- test/fractionstest.py
+++ test/fractionstest.py
@@ -133,6 +133,14 @@ class FractionTest(unittest.TestCase) :
         actual = instance.TeX_str()
         self.assertEqual(expected, actual)
 
+    def test_str_negative(self) :
+        numer = random.randrange(1, 32767)
+        denom = numer + 1
+        instance = src.fractions.Fraction(-numer, denom)
+        expected = "\u2212" + str(numer) + "/" + str(denom)
+        actual = instance.__str__()
+        self.assertEqual(expected, actual)
+    
     def test_constructor_sets_numerator_from_not_lowest_terms(self) :
         expected = random.randrange(1, 1024)
         denom = 3 * expected + 1 + (expected % 2)
