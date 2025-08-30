8664526ec0598b14ab91805f1d66fde4c3309de4
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-29 23:06:12 -0400

    Preparing to fail next test

--- src/fractions.py
+++ src/fractions.py
@@ -43,6 +43,8 @@ class Fraction :
     
     def TeX_str(self) :
         if self.denominator == 1 :
+            if self.numerator < 0 :
+                return "REWIND TO FAILING"
             return str(self.numerator)
         if self.numerator < 0 :
             numer_str = str(-self.numerator)
