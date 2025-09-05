8ce0bc98fed429b0bbb36ffb06031d8a52ef60a2
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-05 15:25:57 -0400

    Corrected __neg__( ) stub

--- src/algnums.py
+++ src/algnums.py
@@ -43,7 +43,7 @@ class AlgebraicInteger :
         return AlgebraicInteger(self.num - subtrahend.num)
 
     # TODO: Write tests for this
-    def __neg__(self, divisor) :
+    def __neg__(self) :
         return self
 
     # TODO: Write tests for this
