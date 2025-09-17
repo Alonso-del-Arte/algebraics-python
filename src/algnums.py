c65725ef5b294422b870f617480b82a5d2af2e7f
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-16 22:27:27 -0400

    Passing TeX_str( ) test

--- src/algnums.py
+++ src/algnums.py
@@ -16,7 +16,7 @@ class AlgebraicInteger :
         return str(self.num)
     
     def TeX_str(self) :
-        return self.__str__()
+        return str(self.num)
     
     def __eq__(self, other) :
         return self.num == other.num
