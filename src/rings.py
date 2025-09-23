b90470ec36623635fb1cb95aa461898b071354ce
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-23 01:11:03 -0400

    Refactored TeX_str( ) further

--- src/rings.py
+++ src/rings.py
@@ -60,6 +60,6 @@ class QuadraticRing(IntegerRing) :
             return "\\textbf Z[\\omega]"
         if self.radicand == 5 :
             return "\\textbf Z[\\phi]"
-        if self.radicand % 4 == 2 or self.radicand % 4 == 3 :
-            return "\\textbf Z[\\sqrt{" + str(self.radicand) + "}]"
-        return "\\mathcal O_{\\textbf Q(\\sqrt{" + str(self.radicand) + "})}"
+        if self.radicand % 4 == 1 :
+            return "\\mathcal O_{\\textbf Q(\\sqrt{" + str(self.radicand) + "})}"
+        return "\\textbf Z[\\sqrt{" + str(self.radicand) + "}]"
