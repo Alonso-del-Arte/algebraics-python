bc502dd1da89b889a2ed8f2560eb54d00bd51437
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-23 01:07:52 -0400

    Failing TeX_str( ) test for d = 3 mod 4 real

--- test/ringstest.py
+++ test/ringstest.py
@@ -238,5 +238,17 @@ class TestQuadraticRing(unittest.TestCase) :
                 self.assertEqual(actual, expected)
             d -= 4
         
+    def test_TeX_str_real_d_3_mod_4(self) :
+        d = 4 * random.randrange(1, 1000) + 3
+        rings_so_far = 0
+        while rings_so_far < self.USUAL_NUMBER_OF_RINGS :
+            if src.numth.squarefree(d) :
+                rings_so_far += 1
+                instance = QuadraticRing(d)
+                expected = "\\textbf Z[\\sqrt{" + str(d) + "}]"
+                actual = instance.TeX_str()
+                self.assertEqual(actual, expected)
+            d += 4
+        
 if __name__ == '__main__' :
     unittest.main()
