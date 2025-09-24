39a03821fca6c847f3b8b39b77e6cd64b4ee2ae4
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-23 23:59:56 -0400

    Passing TeX_blackboardbold_str( ) test for imaginary d = 1 mod 4

--- src/rings.py
+++ src/rings.py
@@ -65,3 +65,9 @@ class QuadraticRing(IntegerRing) :
         if self.radicand % 4 == 1 :
             return "\\mathcal O_{\\textbf Q(\\sqrt{" + str(self.radicand) + "})}"
         return "\\textbf Z[\\sqrt{" + str(self.radicand) + "}]"
+
+    def TeX_blackboardbold_str(self) :
+        if self.radicand > 0 :
+            return "NO TEST YET"
+        return "\\mathcal O_{\\mathbb Q(\\sqrt{" + str(self.radicand) + "})}"
+    
