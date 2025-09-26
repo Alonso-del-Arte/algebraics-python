907444280916066408823813780b33f401f2f277
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-26 10:12:10 -0400

    Passing TeX_blackboardbold_str( ) test for d = 2 mod 4 imaginary

--- src/rings.py
+++ src/rings.py
@@ -67,6 +67,8 @@ class QuadraticRing(IntegerRing) :
         return "\\textbf Z[\\sqrt{" + str(self.radicand) + "}]"
 
     def TeX_blackboardbold_str(self) :
+        if self.radicand % 4 == 2 and self.radicand < 0 :
+            return "\\mathbb Z[\\sqrt{" + str(self.radicand) + "}]"
         if self.radicand == -3 :
             return "\\mathbb Z[\\omega]"
         if self.radicand == 5 :
