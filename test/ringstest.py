56742236b0f2a3cdcb50dea157aa3049bd447dd6
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-26 10:16:49 -0400

    Failing TeX_blackboardbold_str( ) test for d = 2 mod 4 real

--- test/ringstest.py
+++ test/ringstest.py
@@ -304,5 +304,17 @@ class TestQuadraticRing(unittest.TestCase) :
                 self.assertEqual(actual, expected)
             d -= 4
         
+    def test_TeX_blackboardbold_str_real_d_2_mod_4(self) :
+        d = 4 * random.randrange(1, 1000) + 2
+        rings_so_far = 0
+        while rings_so_far < self.USUAL_NUMBER_OF_RINGS :
+            if src.numth.squarefree(d) :
+                rings_so_far += 1
+                instance = QuadraticRing(d)
+                expected = "\\mathbb Z[\\sqrt{" + str(d) + "}]"
+                actual = instance.TeX_blackboardbold_str()
+                self.assertEqual(actual, expected)
+            d += 4
+        
 if __name__ == '__main__' :
     unittest.main()
