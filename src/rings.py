40f24f6e24165834014ec5bc62808f0e9d08746c
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-29 23:45:53 -0400

    Passing TeX_blackboardbold_str( ) test for Gaussian

--- src/rings.py
+++ src/rings.py
@@ -69,6 +69,8 @@ class QuadraticRing(IntegerRing) :
     def TeX_blackboardbold_str(self) :
         if self.radicand % 4 == 2 :
             return "\\mathbb Z[\\sqrt{" + str(self.radicand) + "}]"
+        if self.radicand == -1 :
+            return "\\mathbb Z[i]"
         if self.radicand % 4 == 3 and self.radicand < 0 :
             return "\\mathbb Z[\\sqrt{" + str(self.radicand) + "}]"
         if self.radicand == -3 :
