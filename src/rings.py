c2e03f794ef28089df3612ce60bce008b58a9422
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-02 14:21:36 -0400

    Refactored HTML_str( )

--- src/rings.py
+++ src/rings.py
@@ -82,18 +82,14 @@ class QuadraticRing(IntegerRing) :
     def HTML_str(self) :
         if self.radicand == -1 :
             return "<b>Z</b>[<i>i</i>]"
-        if self.radicand % 4 == 2 :
-            if self.radicand > 0 :
-                return f"<b>Z</b>[&radic;{self.radicand}]"
-            return f"<b>Z</b>[&radic;&minus;{-self.radicand}]"
-        if self.radicand % 4 == 3 :
-            if self.radicand > 0 :
-                return f"<b>Z</b>[&radic;{self.radicand}]"
-            return f"<b>Z</b>[&radic;&minus;{-self.radicand}]"
         if self.radicand == -3 :
             return "<b>Z</b>[&omega;]"
         if self.radicand == 5 :
             return "<b>Z</b>[&phi;]"
+        if self.radicand % 4 == 2 or self.radicand % 4 == 3 :
+            if self.radicand > 0 :
+                return f"<b>Z</b>[&radic;{self.radicand}]"
+            return f"<b>Z</b>[&radic;&minus;{-self.radicand}]"
         if self.radicand > 0 :
             return f"<i>O</i><sub><b>Q</b>(&radic;{self.radicand})</sub>"
         return f"<i>O</i><sub><b>Q</b>(&radic;&minus;{-self.radicand})</sub>"
